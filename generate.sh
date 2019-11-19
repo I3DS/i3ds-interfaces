72e56daeae1722d5fe24f4caefe7c7ae90505f5b
Henrik Austad <henrik.austad@sintef.no>
2019-11-19 13:39:39 +0100

    generate.sh: update script to to sanitize, compile and create .so
    - use asn1.list to generate list files
    - feed all files through the find-bad-word routine
    - compile everything into .c/.h
    - Make all files ready for proper library inclusion by setting include
      to proper path (no local includes any more)
    - Update #defines with same namespace to avoid collisions in the
      include-chain
    - Wrap all files in a new namespace
    - Rename files to corresponding cpp and hpp
    - split .cpp and .hpp into include/<namespace>/ and src/
    - Configure CMake to build a .so and install to a local directory
    - Enable warnings and treat warnings as errors
    - create a tar-archive with headers and .so ready to be installed on
      target, base name on first 12 characters of the git-commit and a
      dirty-tag.

--- generate.sh
+++ generate.sh
@@ -1,38 +1,185 @@
-#! /bin/dash
+#! /bin/bash
+set -e
 
-if [ -z "$ASN1CC" ]; then
-    ASN1CC=$(find $HOME -executable -name asn1.exe -print -quit)
-fi
+full_path=$(realpath "${0}")
+ROOT=$(dirname "${full_path}")
+pushd "${ROOT}" > /dev/null
 
-ROOT=$(dirname $0)
+GENPATH=${ROOT}/generated
+BUILDPATH=${ROOT}/build
+INSTALLPATH=${BUILDPATH}/tmp
+NAMESPACE="i3ds_asn1"
 
-ESROCOS_BASE_DIR="$ROOT/esrocos/types-base/asn"
-I3DS_BASE_DIR="$ROOT/i3ds/types-base/asn"
-I3DS_EXTRA_DIR="$ROOT/i3ds/types-extra/asn"
+cleanup ()
+{
+    # ---------------------------------------------------------------------
+    # Make sure we get a clena build each time
+    test -d "${GENPATH}" && rm -rf "${GENPATH}"
+    mkdir -p "${GENPATH}"
 
-I3DS_ASN1_FILES="$ESROCOS_BASE_DIR/base.asn \
-    $ESROCOS_BASE_DIR/taste-extended.asn \
-    $ESROCOS_BASE_DIR/taste-types.asn \
-    $ESROCOS_BASE_DIR/userdefs-base.asn \
-    $I3DS_BASE_DIR/Analog.asn \
-    $I3DS_BASE_DIR/Common.asn \
-    $I3DS_BASE_DIR/IMU.asn \
-    $I3DS_BASE_DIR/DepthMap.asn \
-    $I3DS_BASE_DIR/PointCloud.asn \
-    $I3DS_BASE_DIR/Sensor.asn \
-    $I3DS_BASE_DIR/Camera.asn \
-    $I3DS_BASE_DIR/Frame.asn \
-    $I3DS_BASE_DIR/LIDAR.asn \
-    $I3DS_BASE_DIR/PoseEstimator.asn \
-    $I3DS_BASE_DIR/Radar.asn \
-    $I3DS_BASE_DIR/Region.asn \
-    $I3DS_BASE_DIR/SampleAttribute.asn \
-    $I3DS_BASE_DIR/StarTracker.asn \
-    $I3DS_BASE_DIR/ToFCamera.asn \
-    $I3DS_EXTRA_DIR/Trigger.asn \
-    $I3DS_EXTRA_DIR/Power.asn \
-    $I3DS_EXTRA_DIR/Flash.asn"
+    test -d "${BUILDPATH}" && rm -rf "${BUILDPATH}"
+    mkdir -p "${BUILDPATH}"
+}
 
-if [ $(command -v $ASN1CC) ]; then
-    $ASN1CC $@ $I3DS_ASN1_FILES
-fi
+run_asn1 ()
+{
+    # ---------------------------------------------------------------------
+    # Find all relevant ASN.1 definitions
+    ASN1_FILES=""
+    while IFS= read -r -d '' file
+    do
+	subsystem=$(dirname "${file}")
+	asnfiles=$(/bin/bash "${file}")
+	for af in ${asnfiles}; do
+	    ASN1_FILES="${ASN1_FILES} ${subsystem}/${af}"
+	done
+    done < <(find . -maxdepth 2 -mindepth 1 -name "asn1.list" -print0)
+
+    # ---------------------------------------------------------------------
+    # basic sanity check, avoid forbidden words
+    for f in ${ASN1_FILES}; do
+	"${ROOT}"/find_illegal_asn1_field_names.py "${f}" || echo "${f} FAILED"
+    done
+
+    # ---------------------------------------------------------------------
+    if [ -z "$ASN1CC" ]; then
+	ASN1CC=$(find "${HOME}" -executable -name asn1.exe -print -quit)
+    fi
+
+    if [ -x "${ASN1CC}" ]; then
+	# https://github.com/koalaman/shellcheck/wiki/SC2086 does not
+	# like this, however, if placed in "", asn1.exe fails to find
+	# the files.
+	command mono "${ASN1CC}" -c -uPER -o "${GENPATH}" ${ASN1_FILES}
+    fi
+}
+
+replace_sym ()
+{
+    sym=$1
+    if [ "${sym}" == "true" ] || \
+    	   [ "${sym}" == "TRUE" ] || \
+    	   [ "${sym}" == "false" ] || \
+    	   [ "${sym}" == "FALSE" ] || \
+    	   [ "${sym}" == "NULL" ]; then
+    	echo "Not replacing ${sym}"
+	return
+    fi
+    echo "${sym} -> ${NAMESPACE}_${sym}"
+    sed -i "s/${sym}/${NAMESPACE}_${sym}/g" -- *
+}
+
+process_generated ()
+{
+    pushd "${GENPATH}" > /dev/null
+    H_FILES=$(ls -- *.h)
+    for f in *.[ch]; do
+	# For each file, match on our h-files and change to proper
+	# includepath and rename to hpp
+	for hf in ${H_FILES}; do
+	    if grep -q "\"${hf}\"" "${f}"; then
+		sed -i "s/#include\ \"${hf}\"/#include\ <${NAMESPACE}\/${hf}pp>/g" "${f}"
+	    fi
+	done
+	# include namespace tag, but only after the #includes
+	# Get last line with inclue (1-indexed)
+	num=$(cat -n "${f}" |grep \#include|tail -n1|awk '{print $1}')
+	num=$((num+1))
+	sed -i "${num}inamespace ${NAMESPACE} {" "${f}"
+    done
+
+
+    # Place closing namespace in different places in c and h
+    while IFS= read -r -d '' file
+    do
+	end=$(cat -n "${file}" | tail -n1 | awk '{print $1}')
+	sed -i "${end}i} // namespace ${NAMESPACE}" "${file}"
+    done < <(find . -maxdepth 1 -name "*.c" -print0)
+
+    while IFS= read -r -d '' file
+    do
+	end=$(cat -n "${file}" | grep \#endif | tail -n1|awk '{print $1}')
+        sed -i "${end}i} // namespace ${NAMESPACE}" "${file}"
+    done < <(find . -maxdepth 1 -name "*.h" -print0)
+
+    # Find all defined constants in hpp, which may (or may not) be
+    # exposed to to others which in turn may lead to define-collisions
+    # Generate a list of all symbols defined:
+    # for sym in $(grep "^\#define" -- *.h|cut -d '(' -f 1 | awk '{print $2}');
+    while IFS= read -r -d '' file
+    do
+    	grep "^\#define" "${file}" | cut -d '(' -f 1 | awk '{print $2}'| while IFS= read -r sym
+    	do
+    	    replace_sym "${sym}"
+    	done
+    done < <(find . -maxdepth 1 -name "*.h" -print0)
+
+    rename "s/h$/hpp/" -- *.h
+    rename "s/c$/cpp/" -- *.c
+
+    mkdir -p include/"${NAMESPACE}"
+    mkdir -p src/
+    mv -- *.hpp include/"${NAMESPACE}"/.
+    mv -- *.cpp src/
+
+    popd > /dev/null
+}
+
+run_cmake ()
+{
+    pushd "${BUILDPATH}" > /dev/null
+    mkdir -p tmp
+    cmake -DCMAKE_INSTALL_PREFIX="${INSTALLPATH}" \
+	  -DCMAKE_INSTALL_INCLUDEDIR="${INSTALLPATH}" \
+	  -DCMAKE_INSTALL_LIBDIR=lib \
+	  -DBUILD_EMBEDDED=OFF \
+	  -DBUILD_DEBUG=ON \
+	  -DCMAKE_INSTALL_INCLUDEDIR=include \
+	  ..
+    make VERBOSE=1 -j8
+    popd > /dev/null
+}
+
+create_bundle ()
+{
+    tarname="${1}"
+    base_tar=$(basename "${tarname}")
+
+    pushd "${BUILDPATH}" > /dev/null
+    make install > /dev/null
+    pushd "${INSTALLPATH}" > /dev/null
+    tar cf "${tarname}" . --owner=root --group=root &>/dev/null
+    echo "   ${base_tar} created"
+    popd > /dev/null #install
+    popd > /dev/null
+}
+
+gen_archive_name()
+{
+    dirty=$(test -z "$(git status -s|grep ^\ M)" || echo "-dirty")
+    hash="$(git log --pretty=oneline -n1|awk '{$hash=substr($1, 0, 12); print $1}')"
+    name="${NAMESPACE}-$(date "+%Y-%m")-${hash}${dirty}.tar"
+    echo "${ROOT}/${name}"
+}
+
+# ---------------------------------------------------------------------
+echo "0. Cleanup"
+cleanup
+
+# ---------------------------------------------------------------------
+echo "1. Generate files from ASN.1 definitions."
+run_asn1
+
+# ---------------------------------------------------------------------
+echo "2. ASN.1 Compilation done, processing for library compilation."
+process_generated
+
+# ---------------------------------------------------------------------
+echo "3. Processing done, kicking cmake to create .so."
+run_cmake
+
+# ---------------------------------------------------------------------
+echo "4. Compile done, creating install-bundle."
+create_bundle "$(gen_archive_name)"
+
+popd > /dev/null # ROOT
